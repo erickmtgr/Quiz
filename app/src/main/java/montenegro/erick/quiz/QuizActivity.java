b707697299ea85fcff35e6564c6a2314b5aa0786
Erick <erickma.1234@gmail.com>
2018-02-13 11:32:20 +0100

    Canviem text de la pregunta desde recursos.

--- app/src/main/java/montenegro/erick/quiz/QuizActivity.java
+++ app/src/main/java/montenegro/erick/quiz/QuizActivity.java
@@ -1,13 +1,31 @@
 package montenegro.erick.quiz;
 
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.RadioButton;
+import android.widget.TextView;
 
 public class QuizActivity extends AppCompatActivity {
+
+    private final int[] ids_boton = {
+            R.id.answer1, R.id.answer2, R.id.answer3, R.id.answer4
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
+
+        TextView question_text = (TextView) findViewById(R.id.question_text);
+        question_text.setText(R.string.question_content);
+
+        String[] answers_texts = getResources().getStringArray(R.array.answers_texts);
+
+        for(int i = 0; i < answers_texts.length; i++){
+            RadioButton rb = (RadioButton) findViewById(ids_boton[i]);
+            rb.setText(answers_texts[i]);
+        }
+
     }
 }
